e4672722fcbe0bbaaea398d990fe4182460b6e08
Juan Fernando <fernando.echavarria@udea.edu.co>
2019-01-20 13:59:22 -0500

    Se añade funcionalidad completa de busqueda

--- Agenda/buscar.php
+++ Agenda/buscar.php
@@ -3,9 +3,65 @@ require_once('db_connect.php');//importamos la conexion
 
     $dinero = $_GET['dinero'];
     $busqueda = $_GET['busqueda'];
+    $nombre = $_GET['nombre'];
+    $cantidad = $_GET['cantidad'];//en Bundle se debia usar una llave que era nombre(creo)
+    $unidad = $_GET['unidad'];
+    $valor = $_GET['valor'];
+    $ano = $_GET['ano'];    
+    $mes = $_GET['mes'];
+    $dia = $_GET['dia'];
     if($busqueda != ''){
         $busqueda = "WHERE tipo='$busqueda'";
     }
+    if($nombre != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND nombre='$nombre'";
+        }else{
+            $busqueda = "WHERE nombre='$nombre'";
+        }
+    }
+    if($cantidad != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND cantidad='$cantidad'";
+        }else{
+            $busqueda = "WHERE cantidad='$cantidad'";
+        }
+    }
+    if($unidad != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND unidad='$unidad'";
+        }else{
+            $busqueda = "WHERE unidad='$unidad'";
+        }
+    }
+    if($valor != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND valor='$valor'";
+        }else{
+            $busqueda = "WHERE valor='$valor'";
+        }
+    }
+    if($ano != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND YEAR(fecha)='$ano'";
+        }else{
+            $busqueda = "WHERE YEAR(fecha)='$ano'";
+        }
+    }    
+    if($mes != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND MONTH(fecha)='$mes'";
+        }else{
+            $busqueda = "WHERE MONTH(fecha)='$mes'";
+        }
+    }        
+    if($dia != ''){
+        if($busqueda != ''){
+            $busqueda = $busqueda." AND DAY(fecha)='$dia'";
+        }else{
+            $busqueda = "WHERE DAY(fecha)='$dia'";
+        }
+    }        
     $sql = "SELECT * FROM tabla2019ingresosT $busqueda;";//generamos el script en sql
     if ($dinero == 'gastos') {
         $sql = "SELECT * FROM tabla2019gastosT $busqueda;";//generamos el script en sql
