0ddb9827ccf1c4524c777790dfc0ff73215b3f44
Juan Fernando Echavarria Zapata <jferchoyes@gmail.com>
2019-08-07 21:39:52 -0500

    fix: Se agrega nueva configuracion de db_connect_template

--- Agenda/db_connect_template.php
+++ Agenda/db_connect_template.php
@@ -1,7 +1,10 @@
 <?php
 //Aqui vamos a declarar las variables de conexion
-header('Content-type: application/json');
-header('Access-Control-Allow-Origin:*');
+header('Content-type: application/json; charset=utf-8');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET');
+header('Access-Control-Allow-Credentials: *');  
+$resultado_enviar = array();
 
 define('DB_USER',"root");//Usuario
 define('DB_PASSWORD',"");//Contraseña
@@ -9,6 +12,7 @@ define('DB_DATABASE',"");//Nombre de la base de datos
 define('DB_SERVER',"localhost");//Nombre o IP del servidor
 define('DB_TABLE_USUARIOS',"tablaUsuarios");
 
+$token = $_GET['token'];
 $nombre = $_GET['nombre'];
 $usuario = $_GET['usuario'];
 $contrasena = $_GET['contrasena'];
@@ -25,9 +29,6 @@ $tipo = $_GET['tipo'];
 $desde = $_GET['desde'];
 $hasta = $_GET['hasta'];
 $pago = $_GET['pago'];
-
-$tablaIngresos = $usuario."_tablaIngresos";
-$tablaGastos = $usuario."_tablaGastos";
 
 //Conexxion
 $con = mysqli_connect(DB_SERVER, DB_USER,DB_PASSWORD,DB_DATABASE) or die ('Unable to Connect');//Aquí se crea la conexion
