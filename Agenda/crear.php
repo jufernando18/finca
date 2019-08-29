b73aabaece28cde0b756c28353ed77b72469a07a
Juan Fernando Echavarria Zapata <jferchoyes@gmail.com>
2019-08-28 21:06:49 -0500

    feat: Se organiza crear.php para que quede protegido contra sql injection y también para que se pueda agregar expresiones con caracteres especiales.

--- Agenda/crear.php
+++ Agenda/crear.php
@@ -6,19 +6,20 @@
         $tablaQuery = $TABLA_GASTOS;
     }
 
-    $start = "UPDATE $tablaQuery SET ";
-    $end = " WHERE idUsuario = $idUsuario AND id = $id";
-    $assignment = "nombre = '$nombre', descripcion = '$descripcion', costo = '$costo', fecha = '$fecha', modificado = now(), tipo = '$tipo'";
     if ($id == null) {
         $start = "INSERT INTO $tablaQuery ";
-        $end = "";
-        $assignment = "(nombre, descripcion, costo, fecha, modificado, tipo, idUsuario) VALUES ('$nombre','$descripcion','$costo','$fecha',now(),'$tipo',$idUsuario)";
+        $assignment = "(nombre, descripcion, costo, fecha, modificado, tipo, idUsuario) VALUES (?,?,?,?, now(),?, $idUsuario)";
+        $stmt = $con->prepare($start.$assignment);
+        $stmt->bind_param("sssss", $nombre, $descripcion, $costo, $fecha, $tipo);
+    } else {
+        $start = "UPDATE $tablaQuery SET ";
+        $end = " WHERE idUsuario = $idUsuario AND id=?";
+        $assignment = "nombre=?, descripcion=?, costo=?, fecha=?, modificado = now(), tipo=?";
+        $stmt = $con->prepare($start.$assignment.$end);
+        $stmt->bind_param("sssssi", $nombre, $descripcion, $costo, $fecha, $tipo, $id);
     }
 
-    $sql = $start.$assignment.$end;
-
-    if(mysqli_query($con, $sql)){
-    } else {
+    if(!$stmt->execute()){
         $resultado_enviar['error']="No se pudo hacer el registro.";
     }
         
