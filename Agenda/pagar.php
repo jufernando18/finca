71e068a22761cc197e7a364ccc917bc8988c9d36
Juan Fernando Echavarria Zapata <jferchoyes@gmail.com>
2019-09-16 23:09:57 -0500

    feat|fix: Se completa inclusion de seguridad para la peticion pagar. Se soluciona error que se estaba presentando con esta funcionalidad.

--- Agenda/pagar.php
+++ Agenda/pagar.php
@@ -7,17 +7,13 @@
     if ($dinero == 'gastos') {
         $tablaQuery = $TABLA_GASTOS;
     } 
-    $sql = "SELECT id, costo FROM $tablaQuery WHERE id=? idUsuario=$idUsuario;";
-    
+    $sql = "SELECT id, costo FROM $tablaQuery WHERE id=? AND idUsuario=$idUsuario;";
     $stmt = $con->prepare($sql);
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($id, $costo);  
     $stmt->fetch();
-    $resultado_enviar['id']=$id;
-    $resultado_enviar['costo']=$costo;
-    echo json_encode($resultado_enviar);
     if($stmt->num_rows != 1) {
         $resultado_enviar['id']=$id;
         $resultado_enviar['costo']=$costo;
@@ -34,7 +30,8 @@
         exit();            
     }
     $costo = explode('-', $costo)[1];
-    $sql = "UPDATE $tablaQuery SET costo='P?|$costo', modificado=now() WHERE id=$id AND idUsuario = $idUsuario;";  
+    $pago = "P$pago|$costo";
+    $sql = "UPDATE $tablaQuery SET costo=?, modificado=now() WHERE id=$id AND idUsuario = $idUsuario;";  
     $stmt = $con->prepare($sql);
     $stmt->bind_param("s", $pago);
     $stmt->execute();
