b37505acc886bd0bafc1c3d2f05d3588cc2bb3c8
Juan Fernando Echavarria Zapata <jferchoyes@gmail.com>
2019-10-01 00:30:37 -0500

    feat: Se integra bootstrap 4. Se inicia modificacion de estilos. Se avanza sin exito en ocultar/mostrar ingreso, registro, informacion, busqueda

--- js/procesar.js
+++ js/procesar.js
@@ -1,7 +1,7 @@
 const BUSCAR_AUTO =true,ACTUALIZAR_BUSQUEDA_AUTO=false,ACTUALIZAR_INGRESO_GASTO_AUTO=true, TIEMPO_NOTIFICACION=1000, TIEMPO_NOTIFICACION_OK=1000, TIEMPO_NOTIFICACION_FAIL=3000, LIMPIAR_AUTO=true,TITULO=true;
 var v_ingreso = new Vue ({el : "#resultadosBusqueda_ingresos",data : {datos: ""}});	
 var v_gasto = new Vue ({el : "#resultadosBusqueda_gastos",data : {datos: ""}});
-var v_entrada = new Vue ({el : "#header",data : {datos: es}});	
+var v_entrada = new Vue ({el : "#header-body",data : {datos: es}});	
 var token;			
 let login = new Login;
 let crud = new Crud;		
@@ -29,4 +29,24 @@ if (ACTUALIZAR_INGRESO_GASTO_AUTO) {
 if (ACTUALIZAR_BUSQUEDA_AUTO){
 	document.getElementById('ano').value = fechaActual.getFullYear();
 	document.getElementById('mes').value = fechaActual.getMonth()+1;
+}
+v_entrada.datos.showIngreso = false;
+v_entrada.datos.showRegistro = false;
+v_entrada.datos.showIngresoInfo = false;
+v_entrada.datos.showBuscar = false;
+function toogleIngreso() {
+	console.log('toogleIngreso: ', v_entrada.datos.showIngreso, ' to: ', !v_entrada.datos.showIngreso);
+	v_entrada.datos.showIngreso = !v_entrada.datos.showIngreso;
+}
+function toogleRegistro() {
+	console.log('toogleRegistro: ', v_entrada.datos.showRegistro, ' to: ', !v_entrada.datos.showRegistro);
+	v_entrada.datos.showRegistro = !v_entrada.datos.showRegistro;
+}
+function toogleIngresoInfo() {
+	console.log('toogleIngresoInfo: ', v_entrada.datos.showIngresoInfo, ' to: ', !v_entrada.datos.showIngresoInfo);
+	v_entrada.datos.showIngresoInfo = !v_entrada.datos.showIngresoInfo;
+}
+function toogleBuscar() {
+	console.log('toogleBuscar: ', v_entrada.datos.showBuscar, ' to: ', !v_entrada.datos.showBuscar);
+	v_entrada.datos.showBuscar = !v_entrada.datos.showBuscar;
 }
