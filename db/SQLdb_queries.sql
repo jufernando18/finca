337f0d934c04c102aa7d6101bd6d95df6f715269
Juan Fernando Echavarria Zapata <jferchoyes@gmail.com>
2020-02-01 14:08:36 -0500

    style: Se añade cabecera a archivo de queries para estadisticas

--- db/SQLdb_queries.sql
+++ db/SQLdb_queries.sql
@@ -1,3 +1,4 @@
+-- QUERIES ESTADISTICAS
 -- Obtener estadisticas por día del mes
 select day(fecha) as diaDelMes, sum(costo) as totalGastos, round(avg(costo)) as promedioValorCosto, min(costo) as costoMinimo, max(costo) as costoMaximo from tablaGastos where costo > 0 and idUsuario = 2 group by day(fecha);
 -- Obtener estadisticas por día de la semana
